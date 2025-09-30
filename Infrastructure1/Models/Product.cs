1f1a0e67a67ffe966c5d5337c43f77371f43568c
estelle <estelle.sossou@yh.nackademin.se>
2025-09-30 14:06:33 +0200

    Add project files.

---
+++ Infrastructure1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Infrastructure1.Models;
+
+public class Product
+{
+    public string Id { get; set; } = null!;
+    public string? ArticleNumber { get; set; }
+    public bool IsService { get; set; }
+    public string ProductName { get; set; } = null!;
+    public string? Description { get; set; } 
+    public string Unit { get; set; } = null!;
+    public decimal UnitPrice { get; set; }
+}
+
+
+public class ProductCreateRequest
+{ 
+}
+
+public class ProductViewRequest
+{ 
+}
---
+++ TestsAssugnment/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestsAssugnment
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
